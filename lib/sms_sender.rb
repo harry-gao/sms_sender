b11105a224c26e826794defa5be14ecd6c6fe7b0
Haiyang (harry) Gao <foxgaocn@gmail.com>
2014-03-24 14:23:07 +1100

    removed the unnecessary puts

--- lib/sms_sender.rb
+++ lib/sms_sender.rb
@@ -22,11 +22,7 @@ class SmsSender
     protocol = @port.to_s == '443' ? 'https' : 'http'
     body = {"origin"=>title, "destination"=>mobile, "message"=>msg }
 
-    puts "#{protocol}://#{@host}#{@request_url}"
-    puts "#{header}"
-    puts "#{body.to_json}"
     response = HTTParty.post("#{protocol}://#{@host}#{@request_url}", headers: header, body: body.to_json)
-    puts "#{response.body}"
     response.code
   end
 
